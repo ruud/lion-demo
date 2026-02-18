eae223afd489f3382fefdc2b9f5d5865adbd6e10
ruud <ruud.prein@gmail.com>
2026-02-18 15:22:09 +0100

    resumable tabs demo

--- lion-demo.js
+++ lion-demo.js
@@ -1,26 +1,35 @@
 import { html, LitElement } from 'lit';
 
-// As a side-effect this way of importing defines the custom elements, eg. <lion-button>, ready for use
-import '@lion/ui/define/lion-button.js';
-import '@lion/ui/define/lion-tooltip.js';
+import '@lion/ui/define/lion-tabs.js';
 
 export class LionDemo extends LitElement {
-  static properties = {
-    header: { type: String },
-    counter: { type: Number },
-  };
-  constructor() {
-    super();
-    this.header = 'Hey dev';
-    this.counter = 0;
-  }
   render() {
     return html`
-      <h1>${this.header}! Increment is at Nr: ${this.counter}</h1>
-      <lion-tooltip has-arrow>
-        <lion-button slot="invoker" @click=${() => {this.counter += 1; console.log(this.counter);}}>increment</lion-button>
-        <span slot="content"> +1 </span>
-      </lion-tooltip>
+    <div class="container">
+      <h1>Lion Tabs Example</h1>
+      
+      <lion-tabs>
+        <button slot="tab">Tab 1: Overview</button>
+        <div slot="panel" class="content">
+          <h2>Welcome to Tab 1</h2>
+          <p>This is the first tab with resumable content. You can click between tabs and the content will persist in the DOM.</p>
+          <my-timer></my-timer>
+          <ajax-provider headers='{"X-Tab": "tab-1", "X-User": "alice"}'>
+            <my-fetcher></my-fetcher>
+          </ajax-provider>
+        </div>
+
+        <button slot="tab">Tab 2: Features</button>
+        <div slot="panel" class="content">
+          <h2>Welcome to Tab 2</h2>
+          <p>This is the second tab with resumable content. You can click between tabs and the content will persist in the DOM.</p>
+          <my-timer></my-timer>
+          <ajax-provider headers='{"X-Tab": "tab-2", "X-User": "bob"}'>
+            <my-fetcher></my-fetcher>
+          </ajax-provider>
+        </div>
+      </lion-tabs>
+    </div>
     `;
   }
 }
